d79252a81dd82f0bbb5de3c5375354ae64bbd7b2
Dayron-Miranda <dayron.miranda20@gmail.com>
2025-11-25 22:48:50 -0500

    PRimer avanze 25

--- PYlatino_financiero/Proyecto_amortizacion.py
+++ PYlatino_financiero/Proyecto_amortizacion.py
@@ -1,2 +1,21 @@
-import panda as pd
+import pandas as pd
 import numpy as np
+import os
+
+#Tasa_interes = 
+
+#def main
+
+
+
+def pedir_info():
+    print ("Proyecto de Amortización de Préstamos")
+    print ("-------------------------------------")
+    print ("Acontinuacion tendremos una serie de preguntas las cuales nos ayudaran a calcular tu credito y te dara una perspectiva de tus pagos mensuales")
+    Tasa_interes = float(input("Por favor ingresa la tasa de interes anual (en porcentaje, por ejemplo 12.5): "))
+    Monto_prestamo = float(input("Por favor ingresa el monto del préstamo: "))
+    Plazo_meses = int(input("Por favor ingresa el plazo del préstamo en meses": "))
+    print("\nCalculando el plan de amortización...\n") 
+    
+
+pedir_info()
